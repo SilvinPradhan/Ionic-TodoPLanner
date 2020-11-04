7ff28f31a7975e0c6e8620c8ed2669078e474207
Silvin Pradhan <silvinpradhan95@gmail.com>
2020-11-04 09:14:21 -0600

    completed the application with styling

--- src/components/Page/TodoForm.component.jsx
+++ src/components/Page/TodoForm.component.jsx
@@ -3,6 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import AddBoxIcon from "@material-ui/icons/AddBox";
+import UpdateIcon from "@material-ui/icons/Update";
 import "./TodoForm.styles.scss";
 
 const useStyles = makeStyles((theme) => ({
@@ -17,19 +18,19 @@ const useStyles = makeStyles((theme) => ({
 export default function TodoForm(props) {
     const classes = useStyles();
 
-    const [input, setInput] = useState("");
+    const [input, setInput] = useState(props.edit ? props.edit.value : "");
 
-    const inputRef = useRef(null)
+    const inputRef = useRef(null);
 
     useEffect(() => {
-        inputRef.current.focus()
-    })
+        inputRef.current.focus();
+    });
 
     const handleSubmit = (e) => {
         e.preventDefault();
         props.onSubmit({
             id: Math.floor(Math.random() * 10000),
-            text: input
+            text: input,
         });
         setInput("");
     };
@@ -49,27 +50,56 @@ export default function TodoForm(props) {
                     autoComplete="off"
                     onSubmit={handleSubmit}
                 >
-                    <TextField
-                        id="filled-secondary"
-                        label="Add a Todo for the planner"
-                        variant="filled"
-                        color="secondary"
-                        type="text"
-                        value={input}
-                        name="text"
-                        className="todo-input"
-                        onChange={handleChange}
-                        ref={inputRef}
-                    />
-                    <Button
-                        style={{ padding: "15px 0px" }}
-                        variant="contained"
-                        color="primary"
-                        type="submit"
-                    >
-                        <AddBoxIcon />
-            Add
-          </Button>
+                    {props.edit ? (
+                        <>
+                            <TextField
+                                id="filled-secondary"
+                                label="Update your Todo"
+                                variant="filled"
+                                color="success"
+                                type="text"
+                                value={input}
+                                name="text"
+                                className="todo-input edit"
+                                onChange={handleChange}
+                                ref={inputRef}
+                            />
+                            <Button
+                                style={{ padding: "15px 0px" }}
+                                variant="contained"
+                                color="primary"
+                                type="submit"
+                                className="edit"
+                            >
+                                <UpdateIcon />
+                Update
+              </Button>
+                        </>
+                    ) : (
+                            <>
+                                <TextField
+                                    id="filled-secondary"
+                                    label="Add a Todo for the planner"
+                                    variant="filled"
+                                    color="secondary"
+                                    type="text"
+                                    value={input}
+                                    name="text"
+                                    className="todo-input"
+                                    onChange={handleChange}
+                                    ref={inputRef}
+                                />
+                                <Button
+                                    style={{ padding: "15px 0px" }}
+                                    variant="contained"
+                                    color="primary"
+                                    type="submit"
+                                >
+                                    <AddBoxIcon />
+                Add
+              </Button>
+                            </>
+                        )}
                 </form>
             </div>
         </>
